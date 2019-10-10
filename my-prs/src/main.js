21e3cab9484560f6621fe198ada001b70a28614e
bo tan <tanboedc@163.com>
2019-10-10 14:01:37 +0800

    第一次项目提交

--- my-prs/src/main.js
+++ my-prs/src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'
+import 'vant/lib/index.css';
+import axios from 'axios'
+Vue.use(Vant)
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
 new Vue({
